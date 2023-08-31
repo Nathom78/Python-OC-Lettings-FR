ddbd2b2fa23cacfad4eb18b0a6b0275e091d71c0
Thomas LECOMTE <99360304+Nathom78@users.noreply.github.com>
2023-08-31 10:40:46 +0200

    Deploy to Azure with Gunicorn server wsgi and whitenoise
    Modification CSS styles.css img background file not found
    
    Signed-off-by: Thomas LECOMTE <99360304+Nathom78@users.noreply.github.com>

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '169.254.129.3']
 
 # Application definition
 
