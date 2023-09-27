2880b5350e0de061d75cfd9fb58b7c84c8dce796
Thomas LECOMTE <99360304+Nathom78@users.noreply.github.com>
2023-09-28 01:55:05 +0200

    Make migrations and migrate
    Signed-off-by: Thomas LECOMTE <99360304+Nathom78@users.noreply.github.com>

--- lettings/migrations/0001_initial.py
+++ lettings/migrations/0001_initial.py
@@ -13,21 +13,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Address',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('number', models.PositiveIntegerField(validators=[django.core.validators.MaxValueValidator(9999)])),
-                ('street', models.CharField(max_length=64)),
-                ('city', models.CharField(max_length=64)),
-                ('state', models.CharField(max_length=2, validators=[django.core.validators.MinLengthValidator(2)])),
-                ('zip_code', models.PositiveIntegerField(validators=[django.core.validators.MaxValueValidator(99999)])),
-                ('country_iso_code', models.CharField(max_length=3, validators=[django.core.validators.MinLengthValidator(3)])),
-            ],
-            options={
-                'verbose_name_plural': 'Addresses',
-            },
-        ),
         migrations.SeparateDatabaseAndState(
             state_operations=[
                 migrations.CreateModel(
@@ -41,32 +26,19 @@ class Migration(migrations.Migration):
                         ('zip_code', models.PositiveIntegerField(validators=[django.core.validators.MaxValueValidator(99999)])),
                         ('country_iso_code', models.CharField(max_length=3, validators=[django.core.validators.MinLengthValidator(3)])),
                     ],
-                    options={
-                        'verbose_name_plural': 'Addresses',
-                    },
                 ),
             ],
             # Table already exists. See oc_lettings_site/migrations/0001_initial.py
             database_operations=[],
-        ),
-        migrations.CreateModel(
-            name='Letting',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=256)),
-                ('address', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='lettings.address')),
-            ],
         ),
         migrations.SeparateDatabaseAndState(
             state_operations=[
                 migrations.CreateModel(
                     name='Letting',
                     fields=[
-                        ('id',
-                         models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                        ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                         ('title', models.CharField(max_length=256)),
-                        ('address',
-                         models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='lettings.address')),
+                        ('address', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='lettings.address')),
                     ],
                 ),
             ],
